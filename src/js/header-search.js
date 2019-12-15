f8d0798d3037d420dff80228b81dcfb574758cb6
nguyenchinhhiep <nguyenchinhhiep95@gmail.com>
2019-12-15 14:20:15 +0700

    header

---
+++ src/js/header-search.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var search = document.getElementById('search-box');
+
+
+function hideSearch(){
+    search.classList.add('show');
+}
+
+
+function showSearch(){
+    search.classList.remove('show');
+}
