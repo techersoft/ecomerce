df251496aaf8cb6b61ba8db53414be93b1e71fd5
lieutoan <mailbombom4@gmail.com>
2019-12-28 16:59:51 +0700

    custome auction

--- src/js/main.js
+++ src/js/main.js
@@ -104,8 +104,30 @@ $(document).ready(function () {
     // Zoom image when hover
     $("#media-image").ezPlus(
         {
-            zoomType: 'inner',
-            cursor: 'crosshair'
+            cursor: 'crosshair',
+            zoomWindowFadeIn: 500,
+            zoomWindowFadeOut: 500,
+            lensFadeIn: 500,
+            lensFadeOut: 500,
+            zoomWindowWidth: 400,
+            zoomWindowHeight: 400
         }
     );
+
+    // Display Image Upload
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('#output').attr('src', e.target.result);
+            }
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+
+    $("#upload").change(function () {
+        readURL(this);
+    });
 });
