143bd2321c490d955faee32d266801a3787f52e9
nguyenchinhhiep <nguyenchinhhiep95@gmail.com>
2020-01-05 02:02:05 +0700

    add dist

--- dist/js/offer.js
+++ dist/js/offer.js
@@ -18,7 +18,29 @@ $(document).ready(function () {
     $('.offer-content-list-supplier').addClass('show-list-details');
     $('.bg-wrapper').addClass('show-bg-wrapper');
   });
+  $('.btn-close-menu-offer').click(function () {
+    $('.offer-content-list-supplier').removeClass('show-list-details');
+    $('.bg-wrapper').removeClass('show-bg-wrapper');
+  });
   $('.btn-see-details').click(function () {
     $('.bg-wrapper-popup').addClass('show-bg-wrapper');
+  });
+  $('#btn-popup-dongnhan').click(function () {
+    $('#popup-dongnhan').addClass('d-block');
+  });
+  $('#btn-popup-relax').click(function () {
+    $('#popup-relax').addClass('d-block');
+  });
+  $('#btn-popup-furnist').click(function () {
+    $('#popup-furnist').addClass('d-block');
+  });
+  $('#btn-cancel-popup-dongnhan').click(function () {
+    $('#popup-dongnhan').removeClass('d-block');
+  });
+  $('#btn-cancel-popup-relax').click(function () {
+    $('#popup-relax').removeClass('d-block');
+  });
+  $('#btn-cancel-popup-furnist').click(function () {
+    $('#popup-furnist').removeClass('d-block');
   });
 });
